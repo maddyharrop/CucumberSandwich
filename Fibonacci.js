0d9e8c38e5ee65f951f5c553ff513edc06b82dc6
Madeline Harrop <madeline.harrop@gmail.com>
2022-08-11 09:30:50 +0100

    First Commit of a fib feature

---
+++ Fibonacci.js
@@ -0,0 +1,19 @@
+class Fibonacci {
+    next() {
+        throw new Error("The next function is not yet supported!");
+    }
+
+    init(seed) {
+        throw new Error("The init function is not yet supported!");
+    }
+
+    skip(forward) {
+        throw new Error("The skip function is not yet supported!");
+    }
+
+    state() {
+        throw new Error("The state function is not yet supported!");
+    }
+}
+
+module.exports = Fibonacci;
---
+++ features/fibonacci.feature
@@ -0,0 +1,5 @@
+Feature: Fibonacci number sequences
+
+    Scenario: Default sequence initiated
+        When a fibonacci sequence is started
+        Then the next number should be 1
